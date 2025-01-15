7fdaac0a12f672335c23c5bcbf69d00e8671107c
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-15 22:23:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function insert_Row() {
     //Write your code here
 	  let table=document.getElementById('sampleTable');
 		// let tr=document.createElement('tr');
-		let content='<tr><td>new Cell1</td> <td>new Cell2</td><tr/>';
+		let content='<tr><td>New Cell1</td> <td>New Cell2</td><tr/>';
 		table.innerHTML=content+table.innerHTML;
   
 }
